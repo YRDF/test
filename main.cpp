273560e98257b8b72c5ada9db9a063e082b1879e
YRDF <zzh5653@163.com>
2024-10-04 18:08:28 +0800

    在分支2保存

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
-
+#include<iostream>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    std::cout<<"切换分支2"<<std::endl;
     w.show();
     return a.exec();
 }
